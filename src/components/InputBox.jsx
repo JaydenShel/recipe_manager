f7b0377bb441eebdadda5141ecadf9f5fb03ea42
jaysh <jshelderfer@ufl.edu>
2024-03-16 14:06:19 -0400

    account info handling

--- src/components/InputBox.jsx
+++ src/components/InputBox.jsx
@@ -1,13 +1,23 @@
-function TextBox({label, type = 'text', required}){
-    return(
+import React from 'react';
+
+function TextBox({ label, type = 'text', required, value, onChange }) {
+    const handleChange = (event) => {
+        if (onChange) {
+            onChange(event.target.value);
+        }
+    };
+
+    return (
         <div>
             <label className="label">{label}:</label>
             <input
-                type = {type}
-                required = {required}
+                type={type}
+                required={required}
+                value={value}
+                onChange={handleChange}
             />
         </div>
-    )
+    );
 }
 
 export default TextBox;
--- src/pages/Account.jsx
+++ src/pages/Account.jsx
@@ -1,16 +1,51 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import '../App.css'
 import Title from "../components/Title";
 import TextBox from "../components/InputBox";
 
 function Account(){
+    const [username, setUsername] = useState("");
+    const [password, setPassword] = useState("");
+    const [error, setError] = useState("");
+
+    const handleSubmit = () => {
+        console.log("Username:", username);
+        console.log("Password:", password);
+        
+        if(username.length === 0 || password.length === 0){
+            setError("Please enter both a username and password!")
+        }
+
+        else if(password.length < 8 || password.length > 16){
+            setError("Please keep password within required length!")
+        }
+
+        else{
+            setError("");
+        }
+    }
+
     return(
         <div>
             <Title/>
             <div>
-                <TextBox label="Choose Username" type="text" required/>
-                <TextBox label="Choose Password" type="text" required/>
-                <h3>*Passwords must be atleast 8 characters in length</h3>
+            <TextBox
+                    label="Choose Username"
+                    type="text"
+                    required
+                    value={username}
+                    onChange={setUsername}
+                />
+                <TextBox
+                    label="Choose Password"
+                    type="password"
+                    required
+                    value={password}
+                    onChange={setPassword}
+                />
+                <h3>*Passwords must be atleast 8-16 characters in length</h3>
+                <button onClick={handleSubmit}>Create Account</button>
+                <h3 className="warning">{error}</h3>
             </div>
         </div>
     );
