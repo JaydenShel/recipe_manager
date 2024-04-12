0de1f7c2259c371f757188bdc6ddc8def0ba9094
jaysh <jshelderfer@ufl.edu>
2024-04-11 21:15:08 -0400

    -

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,7 @@ const app = express();
 const cors = require('cors');
 const bodyParser = require('body-parser');
 const crypto = require('crypto');
+const { Client } = require('pg');
 
 const secretKey = crypto.randomBytes(32).toString('hex');
 global.secretKey = secretKey;
@@ -22,6 +23,14 @@ app.use('/register', registerRoute);
 app.use('/load', loadRoute);
 app.use('/store', storeRoute);
 app.use('/delete', deleteRoute);
+
+const client = new Client({user:'dwhupwkaxbbvpt', host: 'ec2-35-169-9-79.compute-1.amazonaws.com',
+ database:'de6dnkja0sec7f',password:'a12b542886c4e77c11077db38f18c460e43beb4d6de182b6cbfde1fd69fcdf03', port: 5432, ssl: {
+  rejectUnauthorized: false
+ } })
+
+client.connect() .then(() => { console.log('Connected to PostgreSQL database!'); })
+.catch((err) => { console.error('Error connecting to the database:', err); });
 
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
